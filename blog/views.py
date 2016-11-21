50a5e33976681013efebb2721ed88ea92f87e80b
ejeongkim <kkya0428@naver.com>
2016-11-21 22:04:03 +0900

    m_admin_test url만들고, django에서만 코드 입력

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
+def m_admin_test(request):
+
+    adminList = ["hi","admin1@com", "admin2@com", "admin3@com", "admin4@com", "admin5@com", "admin6@com", "admin7@com", "admin8@com", "admin9@com", "admin10@com", "admin11@com", "admin12@com", "admin13@com"]
+
+    if request.method == 'POST':
+        msg_  = request.POST['msg']
+
+        if adminList.__contains__(msg_):
+            return HttpResponse('success')
+        else:
+           return HttpResponse('fail')
--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^m_', include('blog.urls')),
 ]
