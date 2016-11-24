00d3b98c8adabfc8931dc8ae9174581e867ed315
ejeongkim <kkya0428@naver.com>
2016-11-24 21:35:34 +0900

    edit views.py

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,6 @@ import json
 # Create your views here.
 @csrf_exempt
 def m_admin_test(request):
-
-    adminList = ["hi","admin1@com", "admin2@com", "admin3@com", "admin4@com", "admin5@com", "admin6@com", "admin7@com", "admin8@com", "admin9@com", "admin10@com", "admin11@com", "admin12@com", "admin13@com"]
-
     if request.method == 'POST':
     	data = {'foo': 'bar', 'hello': 'world'}
     	return HttpResponse(json.dumps(data), content_type='application/json')
