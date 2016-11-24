3420da38e39934cdc9e814ad39985ef6877f09ca
ejeongkim <kkya0428@naver.com>
2016-11-24 21:56:50 +0900

    edit views.py

--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,11 @@ def m_admin_test(request):
 	adminList = ["hi","admin1@com", "admin2@com", "admin3@com", "admin4@com", "admin5@com", "admin6@com", "admin7@com", "admin8@com", "admin9@com", "admin10@com", "admin11@com", "admin12@com", "admin13@com"]
 
     if request.method == 'POST':
-		msg_  = request.POST['msg']
+		data = {'foo': 'bar', 'hello': 'world', 'ejeong': 'ejeongkim1!'}
+		return HttpResponse(json.dumps(data), content_type='application/json')
+		# msg_  = request.POST['msg']
 
-		if adminList.__contains__(msg_):
-			return HttpResponse('success')
-		else:
-			return HttpResponse('fail')
+		# if adminList.__contains__(msg_):
+		# 	return HttpResponse('success')
+		# else:
+		# 	return HttpResponse('fail')
