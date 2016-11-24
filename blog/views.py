ccf489dd2ec65a7176de895c13f1fa96b163200b
ejeongkim <kkya0428@naver.com>
2016-11-24 21:51:07 +0900

    edit views.py

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,12 @@ import json
 # Create your views here.
 @csrf_exempt
 def m_admin_test(request):
+	adminList = ["hi","admin1@com", "admin2@com", "admin3@com", "admin4@com", "admin5@com", "admin6@com", "admin7@com", "admin8@com", "admin9@com", "admin10@com", "admin11@com", "admin12@com", "admin13@com"]
+
     if request.method == 'POST':
-    	#data = {'foo': 'bar', 'hello': 'world'}
-    	#return HttpResponse(json.dumps(data), content_type='application/json')
-    	return HttpResponse('ejeongkimzhfflsvjtm1!')
+		msg_  = request.POST['msg']
+
+		if adminList.__contains__(msg_):
+			return HttpResponse('success')
+		else:
+			return HttpResponse('fail')
